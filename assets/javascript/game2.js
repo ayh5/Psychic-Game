5b5a715e6c9a9194b9a9ba7184081b394b97a189
Alan Ho <AlanHo@Alans-MacBook-Air.local>
2017-08-02 12:38:29 -0400

    v2

---
+++ assets/javascript/game2.js
@@ -0,0 +1,63 @@
+
+//Initial Variable Setups 
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessesLeft = 9;
+var guessedLetters = [];
+var letterToGuess = null;
+
+
+//Lets the computer select a random letter from the available choices
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+var updateGuessesLeft = function() {
+  /*document.getElementById grabs the HTML element and sets it equal to the guessesLeft.
+   ID 'guessesLeft will be displayed as "Number of Guesses Left:" on page*/
+  document.getElementById('guessesLeft').innerHTML = "Number of Guesses Left: " + guessesLeft;
+};
+
+var updateLetterToGuess = function() {
+  this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
+};
+var updateGuessesSoFar = function() {
+  // Display cumulative user guesses 
+  document.getElementById('guessesSoFar').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
+};
+
+var reset = function() {
+  computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  guesses = 9;
+  guessesLeft = 9;
+  guessedLetters = [];
+  letterToGuess = null;
+  updateLetterToGuess();
+  updateGuessesLeft();
+  updateGuessesSoFar();
+}
+
+updateLetterToGuess();
+updateGuessesLeft();
+
+
+document.onkeyup = function(event) {
+    guessesLeft--;
+  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+  guessedLetters.push(userGuess);
+  updateGuessesLeft();
+  updateGuessesSoFar();
+
+        if (guessesLeft > 0 && userGuess == letterToGuess){
+                wins++;
+                document.querySelector('#wins').innerHTML = "Wins: " + wins;
+                alert("You are a psychic!");
+                reset();
+            } else if(guessesLeft == 0){
+            losses++;
+            document.querySelector('#losses').innerHTML = "Losses: " + losses;
+            alert("Sorry, you are definitely not psychic! However, feel free to try again."); 
+            reset();
+        }
+};
